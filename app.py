08bb6b36e5d9602e1110f0a3dbbc59c50be5c06b
TTVTheAgregado <igorcauan036@gmail.com>
2025-10-22 01:09:23 -0300

    Add: DELETE Product, JSON Response in add_product, add jsonify

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #Importação do Flask
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)   
@@ -18,8 +18,23 @@ class Product(db.Model):
 @app.route('/api/products/add', methods=["POST"])
 def add_product():
     data = request.json
-    product = Product(name=data['name'], price=data['price'], descripition=data.get('descripition'))
-    return data
+    if 'name' in data and 'price' in data:
+        product = Product(name=data['name'], price=data['price'], descripition=data.get('descripition'))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product Added Sucessufol"}), 201
+    return jsonify({"message": "Invalid data"}), 400
+
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id) #Recuperar o Produto pelo ID
+    if product: #Verificar se o produto existe
+        db.session.delete(product) #Deletar o produto do banco de dados
+        db.session.commit() #Confirmar a transação
+        return jsonify({"message": "Product Deleted Successfully"}), 200 
+    return jsonify({"message": "Product Not Found"}), 404
+
 
 
 # Definir uma rota para a página inicial e uma função que será executada ao requisitar
